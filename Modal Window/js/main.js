9343ac8929826a358033041d89a7210bd637329e
satoshi <dev.satoshi.1016@gmail.com>
2023-06-04 18:16:48 +0900

    add: Modal Windowを追加

---
+++ Modal Window/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+{
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    // modal.classList.add('hidden');
+    // mask.classList.add('hidden');
+    close.click();
+  });
+}
+
